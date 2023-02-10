69cefa11b1a0aa1b0162e368ce8d15fbaf950545
syedyus <123729735+syedyus@users.noreply.github.com>
2023-02-10 19:15:53 +0000

    session

---
+++ main/board.go
@@ -0,0 +1,25 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/sprites.atlas\"\n"
+  "default_animation: \"Untitled-1\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale {
+    x: 0.5
+    y: 0.5
+    z: 0.5
+  }
+}
